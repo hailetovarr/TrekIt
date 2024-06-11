58abe4f8c6c6f26732189622aff8733727f0e382
Haile Tovar Romero <hailetovarr@gmail.com>
2024-06-11 09:47:28 -0500

    Initial Commit

---
+++ TrekIt/TrekItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekItApp.swift
+//  TrekIt
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TrekItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
